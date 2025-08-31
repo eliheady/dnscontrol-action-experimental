9658caef0ecfa8abc7082a4bc582f289c8a365cc
Eli Heady <eli.heady@gmail.com>
2025-08-31 14:42:24 -0400

    address shellcheck warnings sanitize fixes and debug

---
+++ bin/shellcheck.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# extract each bash `run` step from action.yml and run it through `bash -n` for
+# validity and shellcheck for safety. requires `yq` and `shellcheck` in $PATH
+
+IFS="
+"
+
+for n in $(yq '.runs.steps[].name' < action.yml )
+do
+  set +e
+  tmpscript=$(mktemp)
+  yq ".runs.steps[] | select(.name == \"$n\") | .run" < action.yml > "$tmpscript"
+  bash -n "$tmpscript" || echo "the script in $n did not pass the validity check"
+  shellcheck --shell bash "$tmpscript" || echo "the script in $n did not pass shellcheck"
+  rm $tmpscript
+  set -e
+done
